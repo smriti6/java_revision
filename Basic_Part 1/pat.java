ceb8f61e7a77e1762ef0b1a2db24e2f2eaf17acb
smriti6 <smritis481@gmail.com>
2022-07-08 23:24:17 +0530

    Basic

---
+++ Basic_Part 1/pat.java
@@ -0,0 +1,8 @@
+public class pat {
+    public static void main(String[] args) {
+        System.out.println("------------");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '-' |");
+    }
+}
