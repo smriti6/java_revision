95166ae2d0cc07da75709be45c23ff8b6931b121
smriti6 <smritis481@gmail.com>
2022-07-17 21:28:48 +0530

    Basic

--- Basic_Part 1/pattern.java
+++ Basic_Part 1/pattern.java
@@ -1,16 +1,18 @@
 public class pattern {
     public static void main(String[] args) {
+        int t=0,z;
         for(int i=0;i<5;i++){
-            for(int j=0;j<5;j++){
-                if(j<i){
-                    System.out.print("*");
-                }
-                else{
-                    System.out.print(" ");
-                }
+            for(int j=i;j<5;j++){
+                System.out.print(" ");
+            }
+            z=t;
+            for(int k=0;k<i;k++){
                 
+                System.out.print(z+" ");
+                z++;
             }
             System.out.println();
+            t++;
         }
     }
 }
