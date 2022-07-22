bb06b3bc9ae10e77092b197fc43c1a246996b6df
smriti6 <smritis481@gmail.com>
2022-07-22 20:22:16 +0530

    Basic

--- Basic_Part 1/array.java
+++ Basic_Part 1/array.java
@@ -15,20 +15,34 @@ public class array {
         //     System.out.println(arr[i]);
         // }
 
-        int arr[]={2,3,4,5};
-        System.out.println("original array");
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
+        // int arr[]={2,3,4,5};
+        // System.out.println("original array");
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.println(arr[i]);
+        // }
+        // int t[]=new int[4];
+        // int j=0;
+        // for(int i=arr.length-1;i>=0;i--){
+        //     t[j]=arr[i];
+        //     j++;
+        // }
+        // System.out.println("After reverse");
+        // for(int i=0;i<t.length;i++){
+        //     System.out.println(t[i]);
+        // }
+
+        //min max element 
+        int arr[]={4,5,6,3,2};
+        int n=arr.length-1;
+        int min=100,max=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
         }
-        int t[]=new int[4];
-        int j=0;
-        for(int i=arr.length-1;i>=0;i--){
-            t[j]=arr[i];
-            j++;
-        }
-        System.out.println("After reverse");
-        for(int i=0;i<t.length;i++){
-            System.out.println(t[i]);
-        }
+        System.out.println("Min = "+min+" max = "+max);
     }
 }
