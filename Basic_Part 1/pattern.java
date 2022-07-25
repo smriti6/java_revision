d8715a5b4b8d86b2b3d4b52d72cd3eac8031acb8
smriti6 <smritis481@gmail.com>
2022-07-25 21:16:43 +0530

    Basic

--- Basic_Part 1/pattern.java
+++ Basic_Part 1/pattern.java
@@ -78,8 +78,31 @@ public class pattern {
             for(int j=i;j<n;j++){
                 System.out.print(" ");
             }
-            for(int j=0;j<i;j++){
+            for(int j=0;j<=i;j++){
                 System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n\n\n");
+
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                if(i==n-1||j==0){
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("\n\n\n");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
