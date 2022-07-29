b8b5491f02381f6adc41030b692480d8387b8b4e
smriti6 <smritis481@gmail.com>
2022-07-29 23:07:28 +0530

    easy

---
+++ easy/palindrom.java
@@ -0,0 +1,19 @@
+public class palindrom {
+    public static void main(String[] args) {
+        int n=121;
+        int o=n;
+        int t=0;
+        while(n!=0){
+            int x=n%10;
+            t=t*10+x;
+            n=n/10;
+        }
+        System.out.println(t);
+        if(o==t){
+            System.out.println("Same");
+        }
+        else{
+            System.out.println("Nope");
+        }
+    }
+}
