0bd2e35a378ded17ab002aa579f8f74907b63a08
smriti6 <smritis481@gmail.com>
2022-08-02 23:25:26 +0530

    easy

---
+++ easy/largeofth.java
@@ -0,0 +1,15 @@
+public class largeofth {
+    public static void main(String[] args) {
+        System.out.println("Largest of 3 number");
+        int a=28,b=14,c=6;
+        if(a>b & a>c){
+            System.out.println("A is greatest ");
+        }
+        else if(b>c){
+            System.out.println("B is greatest");
+        }
+        else {
+            System.out.println("C is  greatest");
+        }
+    }
+}
