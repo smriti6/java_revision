c91e893256bf3cc98db05c5275e9ef12cba5f2ef
smriti6 <smritis481@gmail.com>
2022-08-04 22:47:44 +0530

    easy

---
+++ prime.java
@@ -0,0 +1,12 @@
+public class prime {
+    public static void main(String[] args) {
+        System.out.println("Print number is prime or not");
+        int n=3;
+        if(n%2!=0){
+            System.out.println("Prime");
+        }
+        else {
+            System.out.println("Not Prime");
+        }
+    }
+}
