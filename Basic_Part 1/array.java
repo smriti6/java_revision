17385efcdeefcbf6f3c5f1a1e8fa27f1774e5fb6
smriti6 <smritis481@gmail.com>
2022-08-20 23:35:53 +0530

    easy

--- Basic_Part 1/array.java
+++ Basic_Part 1/array.java
@@ -106,14 +106,30 @@ public class array {
 
     //reverse a array
     int arr[]={1,2,3,4,5,6};
-    int t=0;
-    int tem[]=new int[arr.length];
-    for(int i=arr.length-1;i>=0;i--){
-        tem[t]=arr[i];
-        t++;
+    // int t=0;
+    // int tem[]=new int[arr.length];
+    // for(int i=arr.length-1;i>=0;i--){
+    //     tem[t]=arr[i];
+    //     t++;
+    // }
+    // for(int i=0;i<tem.length;i++){
+    //     System.out.println(tem[i]);
+    // }
+
+
+
+    //method 2 reverse array
+
+    int start=0,end=arr.length-1;
+    while(start<end){
+        int t=arr[start];
+        arr[start]=arr[end];
+        arr[end]=t;
+        start++;
+        end--;
     }
-    for(int i=0;i<tem.length;i++){
-        System.out.println(tem[i]);
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]);
     }
 
     
